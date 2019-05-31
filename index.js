e5cfe2680263f2dd399d85d278165af14c595bae
James Chetwood <james@jameschetwood.co.uk>
2019-05-31 16:44:10 +0100

    blah

--- index.js
+++ index.js
@@ -1,96 +1,48 @@
-const {
-  graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString
-} = require("graphql");
+const { graphql } = require("graphql");
 const { find, filter } = require("lodash");
 const { makeExecutableSchema } = require("graphql-tools");
 
-// const schema = new GraphQLSchema({
-//   query: new GraphQLObjectType({
-//     name: "RootQueryType",
-//     fields: {
-//       hello: {
-//         type: GraphQLString,
-//         resolve() {
-//           return "world";
-//         }
-//       }
-//     }
-//   })
-// });
-//
-// const query = "{ hello }";
-
-const authors = [
-  { id: 1, firstName: "Tom", lastName: "Coleman" },
-  { id: 2, firstName: "Sashko", lastName: "Stubailo" },
-  { id: 3, firstName: "Mikhail", lastName: "Novikov" }
-];
-
-const posts = [
-  { id: 1, authorId: 1, title: "Introduction to GraphQL", votes: 2 },
-  { id: 2, authorId: 2, title: "Welcome to Meteor", votes: 3 },
-  { id: 3, authorId: 2, title: "Advanced GraphQL", votes: 1 },
-  { id: 4, authorId: 3, title: "Launchpad is Cool", votes: 7 }
-];
-
 const resolvers = {
   Query: {
-    posts: () => posts,
-    author: (_, { id }) => find(authors, { id })
-  },
-
-  Mutation: {
-    upvotePost: (_, { postId }) => {
-      const post = find(posts, { id: postId });
-      if (!post) {
-        throw new Error(`Couldn't find post with id ${postId}`);
-      }
-      post.votes += 1;
-      return post;
+    getBooks: () => {
+      return [
+        {
+          title: "Harry Potter",
+          author: {
+            name: "JK Rowling 2"
+          }
+        }
+      ];
+    },
+    getAuthors: () => {
+      return [
+        {
+          name: "JK Rowling",
+          books: [
+            {
+              title: "Harry Potter 2"
+            }
+          ]
+        }
+      ];
     }
-  },
-
-  Author: {
-    posts: author => filter(posts, { authorId: author.id })
-  },
-
-  Post: {
-    author: post => find(authors, { id: post.authorId })
   }
 };
 
 const typeDefs = `
-  type Author {
-    id: Int!
-    firstName: String
-    lastName: String
-    """
-    the list of Posts by this author
-    """
-    posts: [Post]
+  type Query {
+    getBooks: [Book]
+    getAuthors: [Author]
   }
 
-  type Post {
-    id: Int!
+  type Author {
+    name: String
+    books: [Book]
+  }
+
+  type Book {
     title: String
     author: Author
-    votes: Int
-  }
-
-  # the schema allows the following query:
-  type Query {
-    posts: [Post]
-    author(id: Int!): Author
-  }
-
-  # this schema allows the following mutation:
-  type Mutation {
-    upvotePost (
-      postId: Int!
-    ): Post
   }
 `;
 
@@ -101,12 +53,13 @@ const schema = makeExecutableSchema({
 
 const query = `
   query {
-    posts {
-      title
+    getBooks {
+        title
     }
   }
 `;
 
 graphql(schema, query).then(result => {
   console.log(result);
+  console.log(result.data.getBooks[0]);
 });
